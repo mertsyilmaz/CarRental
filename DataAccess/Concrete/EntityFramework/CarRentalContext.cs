ab3e424e31df603745ef624ea60b834f16d8a7d9
mertsyilmaz <36952537+mertsyilmaz@users.noreply.github.com>
2021-02-10 00:51:07 +0100

    Add CarRentalContext and EfCarDal

---
+++ DataAccess/Concrete/EntityFramework/CarRentalContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CarRentalContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarRental;Integrated Security=True;");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+
+
+        public void Add(Car entity)
+        {
+            using (CarRentalContext contex = new CarRentalContext())
+            {
+                var addedEntity = contex.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                contex.SaveChanges();
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (CarRentalContext contex = new CarRentalContext())
+            {
+                var deletedEntity = contex.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                contex.SaveChanges();
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (CarRentalContext contex = new CarRentalContext())
+            {
+                return contex.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (CarRentalContext contex = new CarRentalContext())
+            {
+                return filter == null 
+                    ? contex.Set<Car>().ToList() 
+                    : contex.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (CarRentalContext contex = new CarRentalContext())
+            {
+                var updatedEntity = contex.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                contex.SaveChanges();
+            }
+        }
+    }
+}
