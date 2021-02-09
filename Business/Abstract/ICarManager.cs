e2a5be4f81b9e01c3c8f4347b22b1c44a0d6c955
mertsyilmaz <36952537+mertsyilmaz@users.noreply.github.com>
2021-02-09 19:17:32 +0100

    Add ICarManager and CarManager in Business Layer

--- Business/Abstract/ICarManager.cs
+++ Business/Abstract/ICarManager.cs
@@ -3,14 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Business.Abstract
 {
-    public interface ICarDal
+    public interface ICarManager
     {
         List<Car> GetAll();
         void Add(Car car);
         void Update(Car car);
         void Delete(Car car);
         Car GetById(int id);
+
     }
 }
